42f3c86df6f0a363da6cf513f736b8c6d07cb528
devaftab <web.dev.aftab@gmail.com>
2024-07-07 14:59:22 +0530

    solve db error

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
-console.log(__dirname);
 
 ///deploy end
 
