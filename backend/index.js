7dedbae35918b88d461352a7621392b031141804
devaftab <web.dev.aftab@gmail.com>
2024-07-07 13:42:39 +0530

    hide env file

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
